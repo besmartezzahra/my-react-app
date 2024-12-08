46e500e1054ad44ee684a8bf8a0a87b019b01a09
besmartezzahra <127043088+besmartezzahra@users.noreply.github.com>
2024-12-08 10:58:41 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Tell Next.js this is a standard React app
+  framework: "react",
+}
+
+module.exports = nextConfig
